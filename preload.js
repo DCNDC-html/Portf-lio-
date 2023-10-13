6f01e8e69b67b315db675b7faaedf5f9edbb2425
davi Calo <davinogueirac@alu.ufc.br>
2023-10-13 17:50:11 -0300

    first commit

---
+++ preload.js
@@ -0,0 +1,27 @@
+var sapo, telaJogo,ambu,carroVermelho, carroAmarelo, policia,  carroPrata, sapoObj, telaFim, telaInicio, telaVitoria,  symText;
+
+SAPOW = 1;
+SAPOA = 2;
+SAPOS = 3;
+SAPOD = 4;
+
+function preload() {
+
+  SAPOW = loadImage("assets/sapoW (1).png");
+  SAPOA = loadImage("assets/sapoA(1).png");
+  SAPOS = loadImage("assets/sapoS (1).png");
+  SAPOD = loadImage("assets/sapoD(1).png");
+  
+  
+  telaJogo = loadImage("assets/telaJogo (2).png");
+  telaVitoria =loadImage("assets/telaVitoria.png")
+  ambu = loadImage("assets/ambu.png");
+  carroVermelho = loadImage("assets/carro vermelho.png");
+  carroAmarelo = loadImage("assets/carro amarelo.png");
+  policia = loadImage("assets/policia.png");
+  carroPrata = loadImage("assets/prata.png");
+  sapoObj = loadImage("assets/sapoObjetivo.png");
+  telaFim = loadImage("assets/fim.png");
+  telaInicio = loadImage("assets/telaInicio.png");
+  symText = loadFont("assets/retrogaming.ttf");
+}
